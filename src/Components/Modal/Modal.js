654ba60978ebcee70501cfbb115824246624ed97
Olivier Belzeaux <olivierbelzeaux@gmail.com>
2022-01-19 11:48:32 +0100

    react modal

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -19,7 +19,7 @@ export default function Modal() {
         <div className="overlay">
           <div className="modal">
             <div className="modal-content">
-              <h2>Hello modal</h2>
+              <h2>Hello modal !</h2>
               <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat repellat officia odio accusamus obcaecati est aut modi. Deserunt esse iusto, iste id, ipsam fugit ea eos nobis commodi, officiis tempore dolore dolorum repudiandae recusandae reiciendis sunt laudantium fugiat aspernatur mollitia vitae aut provident repellendus excepturi. Eius illum ex voluptates velit!</p>
               <button
                 onClick={toggleModal}
